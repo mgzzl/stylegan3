df50e0bf836c36c754842c20759e021b2748ea34
mgzzl <max.p.geissler@gmail.com>
2022-08-09 12:59:26 +0200

    add scripts to generate all listed pkls

---
+++ gen_all_im.py
@@ -0,0 +1,76 @@
+import os
+import re
+from typing import List, Optional, Tuple, Union
+
+import click
+import dnnlib
+import numpy as np
+import PIL.Image
+import torch
+
+import legacy
+
+pklPath='C:/Users/hello/Desktop/AI/lucid-sonic-dreams/pytorch/'
+outDir='C:/Users/hello/Desktop/AI/stylegan3/out/'
+seeds=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,17,18,19,20]
+class_idx=0
+truncation_psi=1
+noise_mode='const'
+translate = (0,0)
+rotate = 0
+
+
+pklList = os.listdir(pklPath)
+
+def make_transform(translate: Tuple[float,float], angle: float):
+    m = np.eye(3)
+    s = np.sin(angle/360.0*np.pi*2)
+    c = np.cos(angle/360.0*np.pi*2)
+    m[0][0] = c
+    m[0][1] = s
+    m[0][2] = translate[0]
+    m[1][0] = -s
+    m[1][1] = c
+    m[1][2] = translate[1]
+    return m
+
+for pkl in pklList:
+    pklName = os.path.splitext(pkl)[0]
+    network_pkl = pklPath + pkl
+    outputdir = outDir + pklName
+    vid = outputdir + '/video.mp4'
+    if not os.path.exists(outputdir):
+        os.makedirs(outputdir)
+    print('Loading networks from "%s"...' % network_pkl)
+    device = torch.device('cuda')
+    with dnnlib.util.open_url(network_pkl) as f:
+        G = legacy.load_network_pkl(f)['G_ema'].to(device) # type: ignore
+
+    os.makedirs(outputdir, exist_ok=True)
+
+    # Labels.
+    label = torch.zeros([1, G.c_dim], device=device)
+    if G.c_dim != 0:
+        if class_idx is None:
+            raise click.ClickException('Must specify class label with --class when using a conditional network')
+        label[:, class_idx] = 1
+    else:
+        if class_idx is not None:
+            print ('warn: --class=lbl ignored when running on an unconditional network')
+
+    # Generate images.
+    for seed_idx, seed in enumerate(seeds):
+        print('Generating image for seed %d (%d/%d) ...' % (seed, seed_idx, len(seeds)))
+        z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
+
+        # Construct an inverse rotation/translation matrix and pass to the generator.  The
+        # generator expects this matrix as an inverse to avoid potentially failing numerical
+        # operations in the network.
+        if hasattr(G.synthesis, 'input'):
+            m = make_transform(translate, rotate)
+            m = np.linalg.inv(m)
+            G.synthesis.input.transform.copy_(torch.from_numpy(m))
+
+        img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
+        img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
+        PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outputdir}/seed{seed:04d}.png')
